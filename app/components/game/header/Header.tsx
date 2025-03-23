04b349503aaa1e5e13cede3274db6d758487a944
Santiago Avellaneda Rodríguez <santiagoavellar02@gmail.com>
2025-03-23 00:48:41 -0500

    feat: reorganizing the atributes of the componentS

--- app/components/game/header/Header.tsx
+++ app/components/game/header/Header.tsx
@@ -3,15 +3,26 @@ import ScoreCounter from "./score-counter/ScoreCounter";
 import "./Header.css";
 import Timer from "./timer/Timer";
 
-const Header: React.FC = () => {
+interface HeaderProps {
+  isRunning: boolean;
+  setIsRunning: (isRunning: boolean) => void;
+  player1Score: number;
+  setPlayer1Score: (score: number) => void;
+  player2Score: number;
+  setPlayer2Score: (score: number) => void;
+  minutes: number;
+  setMinutes: (minutes: number) => void;
+  seconds: number;
+  setSeconds: (seconds: number) => void;
+  musicOn: boolean;
+  setMusicOn: (musicOn: boolean) => void;
+  soundEffectsOn: boolean;
+  setSoundEffectsOn: (soundEffectsOn: boolean) => void;
+}
 
-  const [isRunning, setIsRunning] = useState(true);
-  const [player1Score, setPlayer1Score] = useState(20);
-  const [player2Score, setPlayer2Score] = useState(75);
-  const [minutes, setMinutes] = useState(1);
-  const [seconds, setSeconds] = useState(30);
-  const [musicOn, setMusicOn] = useState(true);
-  const [soundEffectsOn, setSoundEffectsOn] = useState(true);
+const Header: React.FC<HeaderProps> = ({isRunning, setIsRunning, player1Score, setPlayer1Score, player2Score, setPlayer2Score, 
+  minutes, setMinutes, seconds, setSeconds, musicOn, setMusicOn, soundEffectsOn, setSoundEffectsOn
+}) => {
 
   return (
     <div className="header">
@@ -30,14 +41,14 @@ const Header: React.FC = () => {
         <button
           type="button"
           aria-label={isRunning ? "Pausar" : "Renaudar"}
-          onClick={() => setIsRunning((prev) => !prev)}
+          onClick={() => setIsRunning(!isRunning)}
         >
           <img src={isRunning ? "/game-screen/header/settings/pause.webp" : "/game-screen/header/settings/continue.webp"} alt="Pausar/Reanudar" />
         </button>
-        <button type="button" aria-label="Musica" onClick={() => setMusicOn((prev) => !prev)}>
+        <button type="button" aria-label="Musica" onClick={() => setMusicOn(!musicOn)}>
           <img src= {musicOn ? "/game-screen/header/settings/music.webp" : "/game-screen/header/settings/no-music.webp"} alt="musica" />
         </button>
-        <button type="button" aria-label="Efectos de sonido" onClick={() => setSoundEffectsOn((prev) => !prev)}>
+        <button type="button" aria-label="Efectos de sonido" onClick={() => setSoundEffectsOn(!soundEffectsOn)}>
           <img src={soundEffectsOn ? "/game-screen/header/settings/effects.webp" : "/game-screen/header/settings/no-effects.webp"} alt="Efectos de sonido" />
         </button>
       </div>
--- app/routes/game/route.tsx
+++ app/routes/game/route.tsx
@@ -1,16 +1,28 @@
-import Header from "../../components/game/header/Header";
-import Board from "../../components/game/board/Board";
+import Header from "~/components/game/header/Header";
+import Board from "~/components/game/board/Board";
 import FruitBar from "~/components/game/footer/fruit-bar/FruitBar";
 import { useState } from "react";
 
 const GameScreen: React.FC = () => {
+  // Header
+  const [isRunning, setIsRunning] = useState(true);
+  const [scorePlayer1, setScorePlayer1] = useState(0);
+  const [scorePlayer2, setScorePlayer2] = useState(0);
+  const [minutes, setMinutes] = useState(1);
+  const [seconds, setSeconds] = useState(30);
+  const [musicOn, setMusicOn] = useState(true);
+  const [soundEffectsOn, setSoundEffectsOn] = useState(true);
+  // FruitBar
   const fruits = ['bananas', 'grapes', 'watermelon', 'orange'];
-  const [score, setScore] = useState(0);
   const [actualFruit, setActualFruit] = useState(fruits[0]);
+
 
   return (
     <div className="game-screen">
-      <Header />
+      <Header isRunning={isRunning} setIsRunning={setIsRunning} player1Score={scorePlayer1} setPlayer1Score={setScorePlayer1} 
+      player2Score={scorePlayer2} setPlayer2Score={setScorePlayer2} minutes={minutes} setMinutes={setMinutes} seconds={seconds}
+      setSeconds={setSeconds} musicOn={musicOn} setMusicOn={setMusicOn} soundEffectsOn={soundEffectsOn} setSoundEffectsOn={setSoundEffectsOn}
+      />
       <Board />
       <FruitBar fruits={fruits} selectedFruit={actualFruit}/>
     </div>
